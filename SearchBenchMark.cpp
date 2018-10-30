33497e2dff59a0c0f5f0822bc3cd25ff6be23a45
Hannibal-ad-Portas <jconroy12@gmail.com>
2018-10-29 21:13:55 -0400

    Binary Search
    We have it it acts like the lenear search

--- SearchBenchMark.cpp
+++ SearchBenchMark.cpp
@@ -19,6 +19,42 @@ int linearSearch (const vector<int> arr, int size, int value) {
 	}
 	ofstream outfile;
 	outfile.open("linearResults.txt", ios::app);
+	cout << "Linear Search Results\n";
+	if (position == -1) {
+		cout << "The value was not found\n";
+		outfile << "The value was not found\n";
+	} else {
+		cout << "The number was found at position " << position << "\n";
+		outfile << "The number was found at position " << position << "\n";
+	}
+	cout << "It took " << comps << " comparisions to find\n";
+	outfile << "It took " << comps << " comparisions to find\n";
+	return position;
+}
+
+int binarySearch(const vector<int> array, int numElems, int value) {
+	int first = 0,
+		last = numElems - 1,
+		middle,
+		position = -1,
+		comps = 0;
+	bool found = false;
+
+	while (!found && first <= last) {
+		middle = (first + last) /2;
+		if (array[middle] == value) {
+			found = true;
+			position = middle;
+		}
+		else if (array[middle] > value)
+			last = middle -1;
+		else
+			first = middle + 1;
+		comps++;
+	}
+	ofstream outfile;
+	outfile.open("binaryResults.txt", ios::app);
+	cout << "Binary Search Results\n";
 	if (position == -1) {
 		cout << "The value was not found\n";
 		outfile << "The value was not found\n";
@@ -43,6 +79,7 @@ void searchBenchDriver () {
 		int value = goodIn(0 -size, size * 2);
 		cout << endl;
 		linearSearch(numbers, size, value);
+		binarySearch(numbers, size, value);
 		exit = exitPrompt();
 	}
 }
